1ecd9b89c64ffbba8cc6c9b5ebc8ff2191525ee3
Abdullah Yahya <MetriclyFrench@users.noreply.github.com>
2017-12-17 15:51:43 -0800

    jQuery will not only run on pages that do not include the class no-js in their body tags

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,9 @@
 'use strict';
+
+$(function () {
+  if ($('body').is('.no-js')){
+    console.log("The class '.no-js' is present on this page. Do not run JS on this Page!");
+  } else {
+    console.log("The class '.no-js' is NOT present on this page. Run JS on this Page!");
+  }
+})
