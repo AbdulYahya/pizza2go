7222152a5452a4fb5982da6bb1327caead1e15cf
Abdullah Yahya <MetriclyFrench@users.noreply.github.com>
2017-12-17 23:16:23 -0800

    Cleaned up code so that it is in a polished, portfolio-quality state

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,41 +1,27 @@
 'use strict';
-// Global Variables
-// var toppings = [];
-var checkedToppings = $('input:checkbox[name=topping]');
-
-// ********** Business Logic **********
-
-// Objects & Prototypes
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
   this.baseCost = 7.95;
   this.baseToppingCost = 0.75;
 }
-
 Pizza.prototype.Cost = function() {
-  if (this.size == 'medium')
+  if (this.size === 'medium') {
     this.baseCost += 2.00;
-  else if (this.size == 'large')
+  } else if (this.size === 'large') {
     this.baseCost += 5.00;
-  else
+  } else {
     this.baseCost;
+  }
 
-  for (var i = 0; i < this.toppings.length; i++) {
-    if (this.toppings[i])
+  for (var index = 0; index < this.toppings.length; index += 1) {
+    if (this.toppings[index]) {
       this.baseCost += this.baseToppingCost;
+    }
   }
 }
-
-// Normal Functions
-function Capitalize(string) {
-  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
-}
-
-function formReset(form) {
-  form.reset();
-}
-
+function Capitalize(string) { return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase(); }
+function formReset(form) { form.reset(); }
 function orderConfirmation() {
   $('.line-up').html('<div class="card">' +
                        '<h4 class="card-header">Order Confirmation</h4>' +
@@ -49,54 +35,34 @@ function orderConfirmation() {
                         '</div>' +
                       '</div>');
 }
-
 function grabToppings(toppings) {
   $('input:checkbox[name=topping]:checked').each(function() {
     toppings.push($(this).val());
   });
 }
-// User Interface Logic
 function pizzaBuilder() {
   var pizzaBuilder = document.getElementById('pizzaBuilder');
-
   pizzaBuilder.addEventListener('submit', function(event) {
-    // Declare preventDefault & stopPropagation first thing
-    // To prevent odd bugs while testing/refactoring
     event.preventDefault();
     event.stopPropagation();
-
     var size = $('#pizzaSize').val();
     var toppings = [];
-
-    // Get toppings
     grabToppings(toppings);
-    // Create order confirmation card
     orderConfirmation();
-    // Instantiating a new Pizza object and storing it into the Var newPizza
     var newPizza = new Pizza(size, toppings);
-    // Append pizza size into order confirmation card
-    $('#orderConfirmation').append('<p class="card-header no-border-header font-weight-light">Base Pizza</p><li class="list-group-item">' + Capitalize(size) + '<span class="badge-pill badge-light float-right">$' + newPizza.baseCost + '</li><p class="card-header no-border-header font-weight-light">Toppings</p>');
-    // Append Toppings into order confirmation card
+    $('#orderConfirmation').append('<p class="card-header no-border-header font-weight-light">Base Pizza</p>' +
+                                   '<li class="list-group-item">' + Capitalize(size) +
+                                   '<span class="badge-pill badge-light float-right">$' + newPizza.baseCost +
+                                   '</li><p class="card-header no-border-header font-weight-light">Toppings</p>');
     $('input:checkbox[name=topping]:checked').each(function() {
-      $('#orderConfirmation').append('<li class="list-group-item">' + Capitalize($(this).val())  + ' <span class="badge-pill badge-light float-right">$ ' + newPizza.baseToppingCost + '</span></li>');
+      $('#orderConfirmation').append('<li class="list-group-item">' +
+                                      Capitalize($(this).val())  +
+                                      ' <span class="badge-pill badge-light float-right">$ ' +
+                                      newPizza.baseToppingCost + '</span></li>');
     });
-    // Call the cost prototype on our newPizza instance
-    // to get the total cost of the uses customized pizza
     newPizza.Cost();
-    // Append the total cost into order confirmation card
     $('#orderTotal').html('$ ' + newPizza.baseCost.toFixed(2));
-    // for (var i = 0; i < toppings.length; i++) {
-    // }
-    // $('#orderConfirmation').append('<li class="list-group-item">' +   + ' <span class="badge-pill badge-info float-right">$ ' + newPizza.baseToppingCost + '</span></li>');
-
-    // $( ":checkbox" ).map(function() { return this.id; }) .get()
-    //   .join();
-    // console.log(newPizza);
-
     formReset(pizzaBuilder);
-    event.preventDefault();
-    event.stopPropagation();
-
   });
 }
 
