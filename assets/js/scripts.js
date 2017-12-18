f13337b18299b4c1941060f1e81df5a39c092700
Abdullah Yahya <MetriclyFrench@users.noreply.github.com>
2017-12-17 17:10:22 -0800

    Added Pizza object and cost prototype

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,9 +3,27 @@
 var toppings = [];
 
 // Business Logic
+function Pizza(size, toppings) {
+  this.size = size;
+  this.toppings = toppings;
+  this.basePrice = 7.95;
+}
+
+Pizza.prototype.Cost = function() {
+  if (this.size == 'medium')
+    this.basePrice += 2;
+  else if (this.size == 'large')
+    this.basePrice += 5;
+  else
+    this.basePrice;
+
+  for (var i = 0; i < this.toppings.length; i++) {
+    if (this.toppings[i])
+      this.basePrice += 0.75;
+  }
+}
 
 // User Interface Logic
-
 function grabToppings() {
   $('input:checkbox[name=topping]').change(function(event) {
     var eventTarget = $(event.target);
@@ -24,7 +42,9 @@ function grabToppings() {
     if (checkboxValue == 'olives') {
       $('#orderConfirmation').append('<li class="list-group-item">' + checkboxValue + '</li>');
     }
+    // console.log(toppings);
   });
+  return toppings;
 }
 
 function pizzaBuilder() {
@@ -32,14 +52,14 @@ function pizzaBuilder() {
 
   pizzaBuilder.addEventListener('submit', function(event) {
     var size = $('#pizzaSize').val();
+    var newPizza = new Pizza(size, toppings);
+    newPizza.Cost()
     console.log(size);
+    console.log(newPizza.basePrice.toFixed(2));
 
     event.preventDefault();
     event.stopPropagation();
   }, grabToppings());
-
-
-
 }
 
 $(function () {
