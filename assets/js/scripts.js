0023d931a633b0c69b8d4b024da97fc5599b0018
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2017-12-19 15:56:38 -0800

    Removed console.log

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -139,11 +139,8 @@ function pizzaBuilder() {
 }
 
 $(function () {
-  if ($('body').is('.no-js')){
-    console.log("The class '.no-js' is present on this page. Do not run JS on this Page!");
-  } else {
+  if (!$('body').is('.no-js')){
     pizzaFormBuilder('#madeToOrderPizza');
     pizzaBuilder();
-    console.log("The class '.no-js' is NOT present on this page. Run JS on this Page!");
   }
 })
