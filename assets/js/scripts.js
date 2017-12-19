39cd8aaf5837745a5f4caf197d9cc1380c15cd46
Abdullah Yahya <33635135+AbdulYahya@users.noreply.github.com>
2017-12-19 15:34:22 -0800

    Refactored code. Order form is now generated using jQuery. Refactored pizza cost prototype into two separate prototypes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,7 @@ function Pizza(size, toppings) {
   this.baseCost = 7.95;
   this.baseToppingCost = 0.75;
 }
-Pizza.prototype.Cost = function() {
+Pizza.prototype.SizeCost = function() {
   if (this.size === 'medium') {
     this.baseCost += 2.00;
   } else if (this.size === 'large') {
@@ -13,7 +13,8 @@ Pizza.prototype.Cost = function() {
   } else {
     this.baseCost;
   }
-
+}
+Pizza.prototype.ToppingsCost = function () {
   for (var index = 0; index < this.toppings.length; index += 1) {
     if (this.toppings[index]) {
       this.baseCost += this.baseToppingCost;
@@ -21,7 +22,6 @@ Pizza.prototype.Cost = function() {
   }
 }
 function Capitalize(string) { return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase(); }
-function formReset(form) { form.reset(); }
 function orderConfirmation() {
   $('.line-up').html('<div class="card">' +
                        '<h4 class="card-header">Order Confirmation</h4>' +
@@ -40,27 +40,99 @@ function grabToppings(toppings) {
     toppings.push($(this).val());
   });
 }
+function formReset(form) { form.reset(); }
+function pizzaFormBuilder(form) {
+  $(form).html('<div class="row">' +
+                  '<div class="col-md-12">' +
+                    '<div class="card-body">' +
+                      '<h4 class="text-left pb-3">Your Name</h4>' +
+                      '<input type="text" class="form-control" id="fName" placeholder="Samewise Gamgee" required>' +
+                      '<div class="invalid-feedback">' +
+                        'Please provide us with your name' +
+                      '</div>' +
+                    '</div>' +
+                  '</div>' +
+                '</div>' +
+                '<div class="row">' +
+                  '<div class="col-md-12">' +
+                    '<div class="card-body">' +
+                      '<h4 class="text-left pb-3">Pick a Size</h4>' +
+                        '<select id="pizzaSize" class="form-control" required>' +
+                          '<option selected value="">Choose...</option>' +
+                          '<option value="small">Small</option>' +
+                          '<option value="medium">Medium</option>' +
+                          '<option value="large">Large</option>' +
+                        '</select>' +
+                    '</div>' +
+                  '</div>' +
+                '</div>' +
+                '<div class="row">' +
+                  '<div class="col-md-12">' +
+                    '<div class="card-body">' +
+                      '<h4 class="text-left pb-3">Pick Your Toppings</h4>' +
+                      '<div class="row" id="top">' +
+                        '<div class="col-md-3 mb-3">' +
+                          '<label class="custom-control custom-checkbox">' +
+                            '<input type="checkbox" class="custom-control-input" name="topping" value="olives" id="olives">' +
+                            '<span class="custom-control-indicator"></span>' +
+                            '<span class="text-muted">Olives</span>' +
+                          '</label>' +
+                        '</div>' +
+                        '<div class="col-md-3 mb-3">' +
+                          '<label class="custom-control custom-checkbox">' +
+                            '<input type="checkbox" class="custom-control-input" name="topping" value="peppers">' +
+                            '<span class="custom-control-indicator"></span>' +
+                            '<span class="text-muted">Peppers</span>' +
+                          '</label>' +
+                        '</div>' +
+                        '<div class="col-md-3 mb-3">' +
+                          '<label class="custom-control custom-checkbox">' +
+                            '<input type="checkbox" class="custom-control-input" name="topping" value="anchovies">' +
+                            '<span class="custom-control-indicator"></span>' +
+                            '<span class="text-muted">Anchovies</span>' +
+                          '</label>' +
+                        '</div>' +
+                        '<div class="col-md-3 mb-3">' +
+                          '<label class="custom-control custom-checkbox">' +
+                            '<input type="checkbox" class="custom-control-input" name="topping" value="onions">' +
+                            '<span class="custom-control-indicator"></span>' +
+                            '<span class="text-muted">Onions</span>' +
+                          '</label>' +
+                        '</div>' +
+                      '</div>' +
+                    '</div>' +
+                  '</div>' +
+                '</div>' +
+                '<button type="submit" class="btn btn-primary">Build</button>');
+}
+
 function pizzaBuilder() {
-  var pizzaBuilder = document.getElementById('pizzaBuilder');
+  var pizzaBuilder = document.getElementById('madeToOrderPizza');
   pizzaBuilder.addEventListener('submit', function(event) {
     event.preventDefault();
     event.stopPropagation();
+
     var size = $('#pizzaSize').val();
     var toppings = [];
     grabToppings(toppings);
     orderConfirmation();
     var newPizza = new Pizza(size, toppings);
+    newPizza.SizeCost();
     $('#orderConfirmation').append('<p class="card-header no-border-header font-weight-light">Base Pizza</p>' +
                                    '<li class="list-group-item">' + Capitalize(size) +
-                                   '<span class="badge-pill badge-light float-right">$' + newPizza.baseCost +
-                                   '</li><p class="card-header no-border-header font-weight-light">Toppings</p>');
+                                     '<span class="badge-pill badge-light float-right">$' + newPizza.baseCost.toFixed(2) +
+                                   '</li>');
+    newPizza.ToppingsCost();
+
+    if (toppings.length > 0) {
+      $('#orderConfirmation').append('<p class="card-header no-border-header font-weight-light">Toppings</p>');
+    }
     $('input:checkbox[name=topping]:checked').each(function() {
-      $('#orderConfirmation').append('<li class="list-group-item">' +
-                                      Capitalize($(this).val())  +
-                                      ' <span class="badge-pill badge-light float-right">$ ' +
-                                      newPizza.baseToppingCost + '</span></li>');
+      $('#orderConfirmation').append('<li class="list-group-item">' + Capitalize($(this).val())  +
+                                       '<span class="badge-pill badge-light float-right">$ ' +
+                                       newPizza.baseToppingCost + '</span>' +
+                                     '</li>');
     });
-    newPizza.Cost();
     $('#orderTotal').html('$ ' + newPizza.baseCost.toFixed(2));
     formReset(pizzaBuilder);
   });
@@ -70,6 +142,7 @@ $(function () {
   if ($('body').is('.no-js')){
     console.log("The class '.no-js' is present on this page. Do not run JS on this Page!");
   } else {
+    pizzaFormBuilder('#madeToOrderPizza');
     pizzaBuilder();
     console.log("The class '.no-js' is NOT present on this page. Run JS on this Page!");
   }
